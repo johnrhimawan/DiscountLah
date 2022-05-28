b89e2492fa91600297fd84b2872d542348c84fe0
Jason Christopher <jason.christopher@u.nus.edu>
2022-05-29 03:19:33 +0800

    Added authentication feature
    Added authentication feature with firebase that supports 'login' and 'signup' functions with firebase.
    
    Three files are added to support the authentication feature, namely `Landing.js`, `Login.js` and `Register.js` under `components` folder.
    
    Installed the following modules:
    - Firebase 8.10.1
    - `@react-navigation/native`
    - `@react-navigation/stack`
    - `@react-native-masked-view/masked-view`
    - `react-native-gesture-handler`
    - `react-native-screens`
    - `react-native-safe-area-view`
    - `react-native-safe-area-context`

--- DiscountLah-app/App.js
+++ DiscountLah-app/App.js
@@ -1,14 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsiPaWC_Q6noWyZ4-j20Zgk8W5_JIvqm4",
+  authDomain: "discountlah-20247.firebaseapp.com",
+  projectId: "discountlah-20247",
+  storageBucket: "discountlah-20247.appspot.com",
+  messagingSenderId: "289678182215",
+  appId: "1:289678182215:web:72bb317a65f39b6f1c2e62",
+  measurementId: "G-WETQBEEE4Q",
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from "./components/auth/Landing"
+import RegisterScreen from "./components/auth/Register"
+
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View>
+          <Text>Loading</Text> 
+        </View>
+      )
+    }
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+    return (
+      <View>
+        <Text> User is logged in! </Text>
+      </View>
+    )
+  }
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +84,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
