f5a8c47abc3a16bbb0336e1b1849198ac5c8b306
johnrhimawan <johnr.himawan@gmail.com>
2022-06-24 23:23:42 +0700

    [BUGGED] Update Add Coupon

--- DiscountLah-app/components/main/Add.js
+++ DiscountLah-app/components/main/Add.js
@@ -1,25 +1,31 @@
 import { View, Button, Text, Modal, SafeAreaView, ActivityIndicator, FlatList } from 'react-native';
 import InlineTextButton from '../feature/InlineTextButton';
 import AppStyles from '../../styles/AppStyles';
-// import { auth, db } from "../../App";
 import firebase from "firebase";
-
-import { collection, addDoc, query, where, getDocs, deleteDoc, doc, setDoc } from "firebase/firestore"; 
-//import { sendEmailVerification } from 'firebase/auth';
+import { collection, addDoc, query, where, getDocs, deleteDoc, doc, setDoc } from "firebase/firestore";
 import React from 'react';
 import AddCouponModal from '../feature/AddCouponModal';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
-export default function AddCoupon({ navigation }) {
+export default function AddCoupon() {
   let [modalVisible, setModalVisible] = React.useState(false);
   let [isLoading, setIsLoading] = React.useState(true);
   let [isRefreshing, setIsRefreshing] = React.useState(false);
   let [addCoupons, setCoupons] = React.useState([]);
+  let [querySnapshot, setQuerySnapshot] = React.useState(null);
+  let [deletedDoc, setDeletedDoc] = React.useState(null);
+  let [docRef, setDocRef] = React.useState(null);
 
   let loadCouponList = async () => {
-    const q = query(collection(db, "coupons"), where("userId", "==", auth.currentUser.uid));
-
-    const querySnapshot = await getDocs(q);
+    console.log("load coupon");
+    const q = query(collection(firebase.firestore(), "coupons"), where("userId", "==", firebase.auth().currentUser.uid));
+    try {
+      let querySnapshot = await getDocs(q);
+    } catch (err) {
+      console.error(err);
+    }
+    setQuerySnapshot(querySnapshot);
+    
     let coupons = [];
     querySnapshot.forEach((doc) => {
       let coupon = doc.data();
@@ -37,17 +43,25 @@ export default function AddCoupon({ navigation }) {
   }
 
   let checkCouponItem = (item, isChecked) => {
-    const couponRef = doc(db, 'coupons', item.id);
+    console.log("check coupon");
+    const couponRef = doc(firebase.firestore(), 'coupons', item.id);
     setDoc(couponRef, { completed: isChecked }, { merge: true });
   };
 
   let deleteCoupon = async (couponId) => {
-    await deleteDoc(doc(db, "coupons", couponId));
+    console.log("delete coupon");
+    try {
+      let deletedDoc = await deleteDoc(doc(firebase.firestore(), "coupons", couponId));
+    } catch (err) {
+      console.error(err);
+    }
+    setDeletedDoc(deletedDoc);
     let updatedCoupons = [...coupons].filter((item) => item.id != couponId);
     setCoupons(updatedCoupons);
   };
 
   let renderCouponItem = ({item}) => {
+    console.log("render coupon");
     return (
       <View style={[AppStyles.rowContainer, AppStyles.rightMargin, AppStyles.leftMargin]}>
         <View style={AppStyles.fillSpace}>
@@ -67,6 +81,7 @@ export default function AddCoupon({ navigation }) {
   }
 
   let showCouponList = () => {
+    console.log("show coupon");
     return (
       <FlatList
         data={coupons}
@@ -81,6 +96,7 @@ export default function AddCoupon({ navigation }) {
   };
 
   let showContent = () => {
+    console.log("show content");
     return (
       <View>
         {isLoading ? <ActivityIndicator size="large" /> : showCouponList() }
@@ -95,13 +111,19 @@ export default function AddCoupon({ navigation }) {
 
 
   let addCoupon = async (coupon) => {
+    console.log("add coupon");
     let couponToSave = {
       text: coupon,
       completed: false,
-      userId: auth.currentUser.uid
+      userId: firebase.auth().currentUser.uid
     };
-    const docRef = await addDoc(collection(db, "coupons"), couponToSave);
 
+    try {
+      let docRef = await addDoc(collection(firebase.firestore(), "coupons"), couponToSave);
+    } catch(err) {
+      console.error(err);
+    }
+    setDocRef(docRef);
     couponToSave.id = docRef.id;
 
     let updatedCoupons = [...coupons];
@@ -110,68 +132,23 @@ export default function AddCoupon({ navigation }) {
     setCoupons(updatedCoupons);
   };
   
-  return (
-    <SafeAreaView>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}>
-        <AddCouponModal 
-          onClose={() => setModalVisible(false)}
-          addCoupon={addCoupon} />
-      </Modal>
-      <Text style={AppStyles.header}>Coupon</Text>
-      {showContent()}
-    </SafeAreaView>
-  )
+  //if (!querySnapshot || !deletedDoc || !docRef) {
+  //  return null;
+ // } else {
+    return (
+      <SafeAreaView>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}>
+          <AddCouponModal 
+            onClose={() => setModalVisible(false)}
+            addCoupon={addCoupon} />
+        </Modal>
+        <Text style={AppStyles.header}>Coupon</Text>
+        {showContent()}
+      </SafeAreaView>
+    )
+  //}
 }
-/*
-import React from "react";
-import { Stylesheet, Text, View, Button } from "react-native";
-// import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
-import {
-  useFonts,
-  Sunflower_500Medium,
-  Sunflower_700Bold,
-} from "@expo-google-fonts/dev";
-export default function Add() {
-  let [fontsLoaded] = useFonts({
-    Sunflower_500Medium,
-    Sunflower_700Bold,
-  });
-  if (!fontsLoaded) {
-    return null;
-  }
-  return (
-    <View style={{marginTop: 20}}>
-      <Text
-        style={{
-          fontFamily: "Sunflower_700Bold",
-          fontSize: 24,
-          textAlign: "left",
-          marginLeft: 10,
-          marginBottom: 5,
-        }}
-      >
-        {" "}
-        Add Coupons{" "}
-      </Text>
-      <Text
-        style={{
-          fontFamily: "Sunflower_500Medium",
-          fontSize: 18,
-          color: "#9f9f9f",
-          textAlign: "left",
-          marginLeft: 15,
-          marginTop: 5,
-          marginBottom: 10,
-        }}
-      >
-        {" "}
-        Fields will be available here for users to add their coupons{" "}
-      </Text>
-    </View>
-  );
-}
-*/
