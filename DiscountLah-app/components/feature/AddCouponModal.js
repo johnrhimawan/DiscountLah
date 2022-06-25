38d8daaa2ffb162ac442b3157a4bdb3ae04dfe8b
johnrhimawan <johnr.himawan@gmail.com>
2022-06-26 02:59:09 +0700

    update fields

--- DiscountLah-app/components/feature/AddCouponModal.js
+++ DiscountLah-app/components/feature/AddCouponModal.js
@@ -4,6 +4,10 @@ import AppStyles from '../../styles/AppStyles';
 
 export default function AddCouponModal(props) {
   let [coupon, setCoupon] = React.useState("");
+  let [store, setStore] = React.useState("");
+  let [description, setDescription] = React.useState("");
+  let [validity, setValidity] = React.useState("");
+
   return (
     <View style={AppStyles.container}>
       <Text style={AppStyles.header}>Add Coupon</Text>
@@ -12,6 +16,21 @@ export default function AddCouponModal(props) {
           placeholder='Coupon'
           value={coupon}
           onChangeText={setCoupon} />
+      <TextInput 
+          style={[AppStyles.textInput, AppStyles.darkTextInput]} 
+          placeholder='Store Name'
+          value={store}
+          onChangeText={setStore} />
+      <TextInput 
+          style={[AppStyles.textInput, AppStyles.darkTextInput]} 
+          placeholder='Coupon Description'
+          value={description}
+          onChangeText={setDescription} />
+      <TextInput 
+          style={[AppStyles.textInput, AppStyles.darkTextInput]} 
+          placeholder='Coupon Validity'
+          value={validity}
+          onChangeText={setValidity} />
       <View style={[AppStyles.rowContainer, AppStyles.rightAligned, AppStyles.rightMargin]}>
         <Button title="Cancel" onPress={props.onClose} />
         <Button title="OK" onPress={() => {
