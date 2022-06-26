4b916b9d7647c64cceab99dc573c803a3dc99f42
johnrhimawan <johnr.himawan@gmail.com>
2022-06-27 00:22:25 +0700

    commit

--- DiscountLah-app/components/main/Add.js
+++ DiscountLah-app/components/main/Add.js
@@ -5,8 +5,7 @@ import {
   Modal,
   SafeAreaView,
   ActivityIndicator,
-  FlatList,
-  StyleSheet
+  FlatList
 } from "react-native";
 import InlineTextButton from "../feature/InlineTextButton";
 import AppStyles from "../../styles/AppStyles";
@@ -24,7 +23,6 @@ import {
 import React from "react";
 import AddCouponModal from "../feature/AddCouponModal";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
-import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function AddCoupon() {
   let [modalVisible, setModalVisible] = React.useState(false);
@@ -34,9 +32,6 @@ export default function AddCoupon() {
   let [querySnapshot, setQuerySnapshot] = React.useState(null);
   let [deletedDoc, setDeletedDoc] = React.useState(null);
   let [docRef, setDocRef] = React.useState(null);
-  let [hasPermission, setHasPermission] = React.useState(null);
-  let [scanned, setScanned] = React.useState(false);
-  let [text, setText] = React.useState('Not yet scanned')
 
   let loadCouponList = async () => {
     console.log("load coupon");
@@ -190,40 +185,6 @@ export default function AddCoupon() {
     setCoupons(updatedCoupons);
   };
 
-  let askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })()
-  }
-
-  // Request Camera Permission
-  React.useEffect(() => {
-    askForCameraPermission();
-  }, []);
-
-  // What happens when we scan the bar code
-  let handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setText(data)
-    console.log('Type: ' + type + '\nData: ' + data)
-  };
-
-  // Check permissions and return the screens
-  if (hasPermission === null) {
-    return (
-      <View style={styles.container}>
-        <Text>Requesting for camera permission</Text>
-      </View>)
-  }
-  if (hasPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ margin: 10 }}>No access to camera</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
-      </View>)
-  }
-
   if (isLoading) {
     return null;
   } else {
@@ -242,39 +203,8 @@ export default function AddCoupon() {
         </Modal>
         <Text style={AppStyles.header}>Coupon</Text>
         {showContent()}
-        <View style={styles.container}>
-      <View style={styles.barcodebox}>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 400, width: 400 }} />
-      </View>
-      <Text style={styles.maintext}>{text}</Text>
-
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
-    </View>
+        
       </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  maintext: {
-    fontSize: 16,
-    margin: 20,
-  },
-  barcodebox: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 300,
-    width: 300,
-    overflow: 'hidden',
-    borderRadius: 30,
-    backgroundColor: 'tomato'
-  }
-});
