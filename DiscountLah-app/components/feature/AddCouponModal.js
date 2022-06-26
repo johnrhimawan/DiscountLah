e4bf5f58e365c973c62d265142ec672c7c0b0af2
johnrhimawan <johnr.himawan@gmail.com>
2022-06-27 01:14:57 +0700

    add datetimepicker in addcouponmodal

--- DiscountLah-app/components/feature/AddCouponModal.js
+++ DiscountLah-app/components/feature/AddCouponModal.js
@@ -1,12 +1,35 @@
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import React from 'react';
 import AppStyles from '../../styles/AppStyles';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 export default function AddCouponModal(props) {
   let [coupon, setCoupon] = React.useState("");
   let [store, setStore] = React.useState("");
   let [description, setDescription] = React.useState("");
-  let [validity, setValidity] = React.useState("");
+  let [date, setDate] = React.useState('');
+  let [isDatePickerVisible, setDatePickerVisibility] = React.useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    setDate(date);
+    hideDatePicker();
+  };
+
+  const getDate = () => {
+    let tempDate = date.toString().split(' ');
+    return date !== ''
+      ? `${tempDate[0]} ${tempDate[1]} ${tempDate[2]} ${tempDate[3]}`
+      : '';
+  };
+
 
   return (
     <View style={AppStyles.container}>
@@ -26,11 +49,20 @@ export default function AddCouponModal(props) {
           placeholder='Coupon Description'
           value={description}
           onChangeText={setDescription} />
-      <TextInput 
-          style={[AppStyles.textInput, AppStyles.darkTextInput]} 
-          placeholder='Coupon Validity'
-          value={validity}
-          onChangeText={setValidity} />
+      <TextInput
+        style={[AppStyles.textInput, AppStyles.darkTextInput]}
+        value={getDate()}
+        placeholder="Set expiry date"
+      />
+      <Button onPress={showDatePicker} title="Set Date" />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+      <Text>{date.toString()}</Text>
+  
       <View style={[AppStyles.rowContainer, AppStyles.rightAligned, AppStyles.rightMargin]}>
         <Button title="Cancel" onPress={props.onClose} />
         <Button title="OK" onPress={() => {
