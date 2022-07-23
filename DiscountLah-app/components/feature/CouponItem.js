0c0e18fa8ac9c91c6cf9157e5f8126023a23ab77
Jason Christopher <jason.christopher@u.nus.edu>
2022-07-24 00:51:57 +0800

    Update Coupon Item Feature
    Included images in the homepage

--- DiscountLah-app/components/feature/CouponItem.js
+++ DiscountLah-app/components/feature/CouponItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { Divider } from "react-native-elements";
+import storeImages from "../data/StoreImages";
 
 const styles = StyleSheet.create({
   menuItemStyle: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    margin: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    marginBottom: 20,
   },
 
   titleStyle: {
@@ -27,7 +30,7 @@ export default function CouponItem({ coupons, img }) {
         <View key={index}>
           <View style={styles.menuItemStyle}>
             <CouponInfo coupon={coupon} />
-            <CouponImage coupon={coupon} marginLeft={10} />
+            <CouponImage coupon={coupon} marginRight={30} />
           </View>
           <Divider
             width={0.5}
@@ -40,23 +43,35 @@ export default function CouponItem({ coupons, img }) {
   );
 }
 
+const getHowManyDays = (date2) => {
+  const today = new Date()
+  const diff = date2 - today
+  const days = Math.floor(diff / (24 * 60 * 60 * 1000))
+  if (days === 0) {
+    return "today"
+  } else if (days === 1) {
+    return "in 1 day"
+  }
+  return "in " + days + " days"
+}
+
 const CouponInfo = (props) => (
   <View style={{ width: 240, justifyContent: "space-evenly" }}>
     <Text style={styles.titleStyle}>{props.coupon.storeName}</Text>
     <Text>{props.coupon.desc}</Text>
-    <Text>{"Valid Until " + timestampToDate(props.coupon.validity.toDate())}</Text>
+    <Text>{"Expires " + getHowManyDays(props.coupon.validity.toDate())}</Text>
   </View>
 );
 
-const CouponImage = ({ marginLeft, ...props }) => (
+const CouponImage = ({ marginRight, ...props }) => (
   <View>
     <Image
-      source={{ uri: props.img }}
+      source={storeImages[props.coupon.storeName]}
       style={{
         width: 100,
         height: 100,
         borderRadius: 8,
-        marginLeft: marginLeft,
+        marginRight: 30
       }}
     />
   </View>
